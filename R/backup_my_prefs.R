320d1d8764cff308b5f5466bc578f15645efee91
Minh-Anh Huynh <minh-anh.huynh@etu.u-paris.fr>
2024-03-21 16:37:56 +0100

    Quick setup

--- R/backup_my_prefs.R
+++ R/backup_my_prefs.R
@@ -1,1 +1,8 @@
-librarian::shelf(RStudioBackupPrefs)
+librarian::shelf("Minh-AnhHuynh/RStudioBackupPrefs")
+
+# Start importing preferences
+start_import_prefs()
+
+# Start github backup
+start_backup_prefs(github_backup = TRUE)
+
