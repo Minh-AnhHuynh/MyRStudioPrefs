6f2d192555875b6b4c5b2f4c66c2a8e406d6ce46
Minh-AnhHuynh <minh-anh.huynh@etu.u-paris.fr>
2024-03-22 15:56:23 +0100

    Update script

--- R/backup_my_prefs.R
+++ R/backup_my_prefs.R
@@ -3,6 +3,8 @@ librarian::shelf("Minh-AnhHuynh/RStudioBackupPrefs")
 # Start importing preferences
 start_import_prefs()
 
+start_import_prefs(pull_github = TRUE)
+
 # Start github backup
 start_backup_prefs(github_backup = TRUE)
 
