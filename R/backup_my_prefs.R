9eb44ae1c9e9842e6d7de9d6065e70b7d46f1bc1
Minh-Anh Huynh <minh-anh.huynh@etu.u-paris.fr>
2025-05-12 18:02:46 +0200

    Auto stash before merge of "main" and "origin/main"

--- R/backup_my_prefs.R
+++ R/backup_my_prefs.R
@@ -1,12 +1,12 @@
-librarian::shelf("Minh-AnhHuynh/RStudioBackupPrefs")
+pacman::p_load_gh("Minh-AnhHuynh/RStudioBackupPrefs")
 
-# Start importing preferences
+# Start importing local preferences
 start_import_prefs()
 
 # Pull from Github
 start_import_prefs(pull_github = TRUE)
 
-# Start github backup
+# Start github backup (export)
 start_backup_prefs(github_backup = TRUE)
 
 # Currently used dark mode themes:
